b464cc1c60dbf12ea1590714843ef39e8c3e08b0
Alexis <108631707+kevinA67@users.noreply.github.com>
2024-03-15 00:25:11 -0600

    Cuatro

--- app/src/main/java/com/example/pm2e2grupo6/ActivityCreate.java
+++ app/src/main/java/com/example/pm2e2grupo6/ActivityCreate.java
@@ -2,13 +2,40 @@ package com.example.pm2e2grupo6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+import android.widget.VideoView;
 
 public class ActivityCreate extends AppCompatActivity {
 
+
+    Button contactos, salvarContacto;
+    EditText nombre, telefono, latitud, longitud;
+    VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        contactos=(Button) findViewById(R.id.btnContactos);
+        salvarContacto=(Button) findViewById(R.id.btnSalvar);
+        nombre=(EditText) findViewById(R.id.txtNombre);
+        telefono=(EditText) findViewById(R.id.txtTelefono);
+        latitud=(EditText) findViewById(R.id.txtLatitud);
+        longitud=(EditText) findViewById(R.id.txtLongitud);
+        videoView=(VideoView) findViewById(R.id.videoView);
+
+
+        contactos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), ActivityLista.class);
+                startActivity(intent);
+            }
+        });
     }
 }
